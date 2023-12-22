b5ab871ce92b8c93197915cc151086753e94f463
Karly Berrning <berningkarly@Karlys-MacBook-Air.local>
2023-12-21 20:22:20 -0600

    kinda completed studio

--- inheritance/testing-inheritance/studio/src/main/org/launchcode/MultipleChoice.java
+++ inheritance/testing-inheritance/studio/src/main/org/launchcode/MultipleChoice.java
@@ -7,13 +7,15 @@ public class MultipleChoice extends Questions {
     Scanner input = new Scanner(System.in);
     public MultipleChoice(String question, String answer) {
         super(question, answer);
-        if (Objects.equals(userAnswer, newAnswer)) {
-            System.out.println("Correct");
-        } else {
-            System.out.println("Incorrect");
+        public MultipleChoiceCheck {
+            if (Objects.equals(userAnswer, newAnswer)) {
+                System.out.println("Correct");
+            } else {
+                System.out.println("Incorrect");
+            }
         }
     }
-
+String question = new Questions("Which coding language is the hardest?", "Java");
     String newAnswer = getAnswer();
     String userAnswer = input.nextLine();
 }
